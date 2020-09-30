a7ba62c81a21a0da016cb5b9c98d6ac73144891d
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-09-30 13:57:02 -0700

    added reverseArrayUsingUnshift , and reverseArrayUsingPop functions

--- array-ds.challenge.js
+++ array-ds.challenge.js
@@ -24,11 +24,48 @@
 */
 
 
-// Complete the reverseArray function below.
-function reverseArray(a) {
+let testArray1 = [1,2,3];
+let testArray2 = [6,7,8];
 
 
+
+
+// Write a function that reverse array with unshift
+// Note: unshift has a time complexity of 0(n)
+function reverseArrayUsingUnshift(a) {
+    let newArr = [];
+
+    for(let i = 0; i < a.length; i++) {
+        newArr.unshift(a[i]);
+    }
+
+    return newArr;
 }
 
 
-reverseArray();
+
+
+/* 
+
+Write a function that reverses array with pop method
+    Note: Array.pop() has a time complexity of 0(1)
+
+    1. Initialize a new array
+    2. Iterate over that passed in array, starting from that last index of that array
+    3. Push every item in the passed in array, started from the last index into the new array
+    4. Return the new array
+ 
+ */
+function reverseArrayUsingPop(a){
+    let newArr = [];
+    
+    for(let i = 0; i < a.length; i++) {
+        newArr.push(a[a.length - i - 1]);
+    }
+
+    return newArr;
+};
+
+
+reverseArrayUsingUnshift(testArray1);
+reverseArrayUsingPop(testArray2);
